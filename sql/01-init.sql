ffa466f1d9e0f47bf209160679cebed03145433a
MatweyL <matweyka2003@mail.ru>
2022-10-13 19:20:45 +0300

    added proxying

--- sql/01-init.sql
+++ sql/01-init.sql
@@ -5,38 +5,19 @@ FLUSH PRIVILEGES;
 
 USE appDB;
 CREATE TABLE IF NOT EXISTS users (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
-  surname VARCHAR(40) NOT NULL,
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  login VARCHAR(20) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
   PRIMARY KEY (ID)
 );
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
+CREATE TABLE IF NOT EXISTS notes (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  title VARCHAR(128) NOT NULL,
+  body TEXT NOT NULL,
+  creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (ID)
+);
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Bob', 'Marley') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Bob' AND surname = 'Marley'
-) LIMIT 1;
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
-
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Kate', 'Yandson') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Kate' AND surname = 'Yandson'
-) LIMIT 1;
-
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Lilo', 'Black') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Lilo' AND surname = 'Black'
-) LIMIT 1;
+INSERT INTO users(login, password_hash) VALUES ("matvey", "{SHA}0DPiKuNIrrVmD8IUCuw1hQxNqZc=");
